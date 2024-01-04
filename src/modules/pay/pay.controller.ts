c96fca81d1351775ca3414a8b337400d179f970b
Waly <debianw@gmail.com>
2024-01-04 14:19:25 -0600

    pay/charge endpoint

--- src/modules/pay/pay.controller.ts
+++ src/modules/pay/pay.controller.ts
@@ -1,11 +1,24 @@
+import { FastifyInstance } from 'fastify'
 import * as payService from './pay.service'
 
-async function routes(fastify, options) {
+async function routes(fastify: FastifyInstance, options) {
   fastify.get('/', async (request, reply) => {
     const payments = payService.getPayments() 
 
     return {
       payments,
+    }
+  })
+
+  fastify.post<{
+    Body: {
+      amount: number
+    }
+  }>('/charge', async(req, rep) => {
+    const { amount } = req.body
+
+    return {
+      amount
     }
   })
 }
